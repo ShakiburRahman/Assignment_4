2eac3e58f748cf3afac76781a4d983333156535a
Shakiburrahman <syed.rahman03@northsouth.edu>
2018-12-25 10:56:42 +0600

    changed bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,1 +1,27 @@
+#include "bubbleSort.h"
+#include<iostream>
 
+using namespace std;
+
+void bubbleSort:: BubbleSorter(int *array, int size){
+    bool flag = true;
+    int number= 0;
+    int temp;
+    while (flag){
+        flag= false;
+        number++;
+        for(int index=0; index<size-number; ++index ){
+            if(array[index]> array[index+1]){
+                temp= array[index];
+                array[index]= array[index+1];
+                array[index+1]=temp;
+
+                for(int index=0;index<8; index++ ){
+                    cout<< array[index]<<" ";
+                }
+                cout<<""<<endl;
+                flag= true;
+            }
+        }
+    }
+}
