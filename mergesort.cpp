c76114456adb7389a10eb30a0ba0c10a45a37fd0
Shakiburrahman <syed.rahman03@northsouth.edu>
2018-12-26 11:29:25 +0600

    changed mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,1 +1,38 @@
+#include "mergesort.h"
+#include<iostream>
 
+using namespace std;
+
+void MergeSorter(int arr[],int arr_size){
+int *temp= new int[high-low+1];
+int index= low, key=mid+1;
+int lead=0;
+
+while(index <= mid && key <=high)
+   {
+       if(arr[index] <= arr[key])
+           temp[lead++] = arr[index++];
+       else
+           temp[lead++] = arr[key++];
+   }
+while(index <= mid)
+       temp[lead++] = arr[index++];
+while(key<= high)
+       temp[lead++] = arr[key++];
+   for(lead= 0, index= low; index <= high; ++index, ++lead)
+       arr[index] = temp[lead];
+   delete []temp;
+   for(int index=0; index<8; index++)
+   {
+       cout<<arr[index]<<" ";
+   }
+   cout<<""<<endl;
+
+
+
+
+
+
+
+
+}
