8a229d54e2fd5172f73df8b351a22615c9cced09
Shakiburrahman <syed.rahman03@northsouth.edu>
2018-12-26 12:41:00 +0600

    changed selectionsort.h

--- selectionsort.h
+++ selectionsort.h
@@ -1,5 +1,16 @@
 #ifndef SELECTIONSORT
 #define SELECTIONSORT
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class selectionSort{
+
+public:
+      void SelectionSorter(int arr[], int arr_size);
+      void SelectioSorterVector(vector<int> & data);
+
+   };
 
 #endif // SELECTIONSORT
 
